72654b4734bcaa52711f53894329c9642744a6dd
baluchowdary <bhardwaja.kollu@gmail.com>
2021-10-21 02:34:32 +0530

    Added Eureka Server Functionality

--- src/main/java/com/kollu/customer/CustomerModuleApplication.java
+++ src/main/java/com/kollu/customer/CustomerModuleApplication.java
@@ -2,8 +2,10 @@ package com.kollu.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerModuleApplication {
 
 	public static void main(String[] args) {
