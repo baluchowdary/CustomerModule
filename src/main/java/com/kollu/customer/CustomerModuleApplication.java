0e670fda50d6a1e01ce79708acaef40fded1e204
baluchowdary <bhardwaja.kollu@gmail.com>
2021-10-22 11:50:50 +0530

    Added Swagger Documentation Feature

--- src/main/java/com/kollu/customer/CustomerModuleApplication.java
+++ src/main/java/com/kollu/customer/CustomerModuleApplication.java
@@ -4,10 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients("com.kollu.customer")
+@EnableSwagger2
 public class CustomerModuleApplication {
 
 	public static void main(String[] args) {
@@ -15,4 +24,15 @@ public class CustomerModuleApplication {
 		SpringApplication.run(CustomerModuleApplication.class, args);
 	}
 
+	@Bean
+	   public Docket productApi() {
+	      return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+	         .apis(RequestHandlerSelectors.basePackage("com.kollu.customer")).build();
+	   }
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Welcome! Customer Micro Service")
+				.description("Customer Micro Service Swagger Document")
+				.build();
+	}
 }
