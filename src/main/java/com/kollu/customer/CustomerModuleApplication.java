93abc4cc4e5546e3efbe82b4d719fc7d2ca8ba91
baluchowdary <bhardwaja.kollu@gmail.com>
2021-10-24 11:58:38 +0530

    Updated project name from uppercase to lowercase and Generating custom images

--- src/main/java/com/kollu/customer/CustomerModuleApplication.java
+++ src/main/java/com/kollu/customer/CustomerModuleApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
-import brave.sampler.Sampler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -50,10 +49,10 @@ public class CustomerModuleApplication {
 	
 /*Distributed Tracing/Zipkin*/
 	
-	@Bean
+	/*@Bean
 	public Sampler defaultSampler() {
 		System.out.println("Console:: CustomerModuleApplication - defaultSampler method");
 		logger.info("CustomerModuleApplication - defaultSampler method");
 	    return Sampler.ALWAYS_SAMPLE;
-	}
+	}*/
 }
